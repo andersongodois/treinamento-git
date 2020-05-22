93cd64fc2e07be0001511675a6902ca50f2554e6
andersongodois <anderson_godois@hotmail.com>
2020-05-22 08:28:03 -0300

    commit

--- HelloWorld/src/br/com/example/git/ArquivoAlterado.java
+++ HelloWorld/src/br/com/example/git/ArquivoAlterado.java
@@ -3,5 +3,6 @@ package br.com.example.git;
 public class ArquivoAlterado {
 	public static void main(String[] args) {
 		System.out.println("Alterado");
+		System.out.println("Alteração para o merge!");
 	}
 }
